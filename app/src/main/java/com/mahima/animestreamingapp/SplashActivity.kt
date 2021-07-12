4c1ccaf2fe385cba47cb66d3c6c405e5c222a197
mahima shrestha <mahimashrestha43@gmail.com>
2021-07-13 01:24:03 +0545

    Logic for splash activity added

--- app/src/main/java/com/mahima/animestreamingapp/SplashActivity.kt
+++ app/src/main/java/com/mahima/animestreamingapp/SplashActivity.kt
@@ -1,11 +1,28 @@
 package com.mahima.animestreamingapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+//        using coroutine for splash activity
+        CoroutineScope(Dispatchers.Main).launch{
+//            suspend for the splash activity
+            delay(2000)
+//            open intent activity
+            startActivity(Intent(this@SplashActivity,LoginActivity::class.java))
+//            finish or destroy the splash screen
+            finish()
+
+
+        }
     }
 }
