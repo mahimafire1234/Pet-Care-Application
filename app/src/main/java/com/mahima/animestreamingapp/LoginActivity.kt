47af70972a84c616a03e8a0adcb3962429932bea
mahima shrestha <mahimashrestha43@gmail.com>
2021-07-15 16:28:16 +0545

    animation for splash screen added

--- app/src/main/java/com/mahima/animestreamingapp/LoginActivity.kt
+++ app/src/main/java/com/mahima/animestreamingapp/LoginActivity.kt
@@ -1,11 +1,20 @@
 package com.mahima.animestreamingapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var tvsignup:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        tvsignup=findViewById(R.id.tvsignuplink)
+
+        tvsignup.setOnClickListener {
+            startActivity(Intent(this@LoginActivity,SignupActivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/mahima/animestreamingapp/SplashActivity.kt
+++ app/src/main/java/com/mahima/animestreamingapp/SplashActivity.kt
@@ -3,26 +3,46 @@ package com.mahima.animestreamingapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
+import android.widget.RelativeLayout
+import android.widget.TextView
+import de.hdodenhof.circleimageview.CircleImageView
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class SplashActivity : AppCompatActivity() {
+    private lateinit var logo: CircleImageView
+    private lateinit var tvtext: TextView
+    private lateinit var rel: RelativeLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        logo=findViewById(R.id.logo)
+        tvtext=findViewById(R.id.tvtext)
+        rel=findViewById(R.id.rel)
+        //        for animation
+
+        val animation=AnimationUtils.loadAnimation(this,R.anim.top_animation)
+        val animation1=AnimationUtils.loadAnimation(this,R.anim.bottom_animation)
+        logo.setAnimation(animation)
+        tvtext.setAnimation(animation1)
+
+
+
+
 //        using coroutine for splash activity
         CoroutineScope(Dispatchers.Main).launch{
 //            suspend for the splash activity
-            delay(2000)
+            delay(5000)
 //            open intent activity
             startActivity(Intent(this@SplashActivity,LoginActivity::class.java))
 //            finish or destroy the splash screen
             finish()
-
-
         }
+
     }
 }
+
