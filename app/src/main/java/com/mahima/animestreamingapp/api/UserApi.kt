fc167bf069e1adbb0f8e950f75f98b3e1d631a61
mahima shrestha <mahimashrestha43@gmail.com>
2021-08-18 12:39:46 +0545

    Created api for registering user

--- app/src/main/java/com/mahima/animestreamingapp/api/UserApi.kt
+++ app/src/main/java/com/mahima/animestreamingapp/api/UserApi.kt
@@ -1,4 +1,15 @@
 package com.mahima.animestreamingapp.api
+
+import com.mahima.animestreamingapp.model.UserModel
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
+
 //this is the api for user log in and register
 interface UserApi {
+//    api for registering user
+    @POST("users/signin")
+    suspend fun RegisterUser(
+        @Body user : UserModel
+    ):Response<UserResponse>
 }
