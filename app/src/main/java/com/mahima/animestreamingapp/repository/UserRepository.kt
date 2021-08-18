4872736be75f2dab90111ef5e5d0f78a8c8e35dd
mahima shrestha <mahimashrestha43@gmail.com>
2021-08-18 13:37:12 +0545

    Created a userapi variable

--- app/src/main/java/com/mahima/animestreamingapp/repository/UserRepository.kt
+++ app/src/main/java/com/mahima/animestreamingapp/repository/UserRepository.kt
@@ -1,4 +1,9 @@
 package com.mahima.animestreamingapp.repository
 
+import com.mahima.animestreamingapp.api.ServiceBuilder
+import com.mahima.animestreamingapp.api.UserApi
+
+//this file is for user repository
 class UserRepository {
+    private val userApi =ServiceBuilder.buildService(UserApi::class.java)
 }
