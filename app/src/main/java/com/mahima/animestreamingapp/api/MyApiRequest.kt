f9082736d63492c4643a89b951885b2af547b524
mahima shrestha <mahimashrestha43@gmail.com>
2021-08-18 12:36:10 +0545

    successfully created api request class

--- app/src/main/java/com/mahima/animestreamingapp/api/MyApiRequest.kt
+++ app/src/main/java/com/mahima/animestreamingapp/api/MyApiRequest.kt
@@ -1,4 +1,29 @@
 package com.mahima.animestreamingapp.api
 
+import org.json.JSONException
+import org.json.JSONObject
+import retrofit2.Response
+import java.io.IOException
+
 abstract class MyApiRequest {
+    suspend fun <T> apiRequest(call: suspend () -> Response<T>) : T{
+//        response variable
+        val response = call.invoke()
+        if(response.isSuccessful){
+            return response.body()!!
+        }else{
+            val error = response.errorBody()?.string()
+            val message = StringBuilder()
+            error?.let {
+                try{
+                    message.append(JSONObject(it).getString("success"))
+                }catch (e:JSONException){
+
+                }
+                message.append("\n")
+            }
+            message.append("Error code : ${response.code()}")
+            throw IOException(error)
+        }
+    }
 }
