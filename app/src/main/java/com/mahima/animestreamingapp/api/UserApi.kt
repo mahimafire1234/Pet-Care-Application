2b37703f98042d2146ac3686f02a4cc33b02c3b3
mahima shrestha <mahimashrestha43@gmail.com>
2021-08-18 12:42:24 +0545

    Created a user response class

--- app/src/main/java/com/mahima/animestreamingapp/api/UserApi.kt
+++ app/src/main/java/com/mahima/animestreamingapp/api/UserApi.kt
@@ -1,6 +1,7 @@
 package com.mahima.animestreamingapp.api
 
 import com.mahima.animestreamingapp.model.UserModel
+import com.mahima.animestreamingapp.response.UserResponse
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
---
+++ app/src/main/java/com/mahima/animestreamingapp/response/UserResponse.kt
@@ -0,0 +1,6 @@
+package com.mahima.animestreamingapp.response
+//this data class is for the repsonse
+data class UserResponse (
+    val token : String ? = null,
+     val success : Boolean?= null
+)
