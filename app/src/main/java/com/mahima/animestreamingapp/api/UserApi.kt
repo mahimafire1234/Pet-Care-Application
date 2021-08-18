a153e1984bb32c3c00ddd2b9a208db66b14b3bf3
mahima shrestha <mahimashrestha43@gmail.com>
2021-08-18 14:53:04 +0545

    Created api for login

--- app/src/main/java/com/mahima/animestreamingapp/api/UserApi.kt
+++ app/src/main/java/com/mahima/animestreamingapp/api/UserApi.kt
@@ -4,6 +4,8 @@ import com.mahima.animestreamingapp.model.UserModel
 import com.mahima.animestreamingapp.response.UserResponse
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
 
 //this is the api for user log in and register
@@ -13,4 +15,11 @@ interface UserApi {
     suspend fun RegisterUser(
         @Body user : UserModel
     ):Response<UserResponse>
+//    api for signning up users
+    @FormUrlEncoded
+    @POST("login")
+    suspend fun LoginUser(
+        @Field("email") email: String ,
+        @Field("password") password :String
+    )
 }
