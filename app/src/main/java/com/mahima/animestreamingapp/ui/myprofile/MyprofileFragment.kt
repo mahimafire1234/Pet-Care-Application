882fc7da48151d42bb3bb857b05188959cde8089
mahima shrestha <mahimashrestha43@gmail.com>
2021-08-23 23:02:37 +0545

    Added about us

--- app/src/main/java/com/mahima/animestreamingapp/ui/myprofile/MyprofileFragment.kt
+++ app/src/main/java/com/mahima/animestreamingapp/ui/myprofile/MyprofileFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import android.widget.TextView
 import androidx.lifecycle.Observer
 import com.mahima.animestreamingapp.R
@@ -20,6 +22,8 @@ class MyprofileFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private val items = arrayOf("Name","Edit","About Us","Terms and Conditions","Log out")
+    private lateinit var listview : ListView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,10 +36,16 @@ class MyprofileFragment : Fragment() {
         _binding = MyprofileFragmentBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textMyprofile
-        myprofileViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+//        for listview
+        listview = root.findViewById(R.id.listview)
+//        adapter
+        val adapter = ArrayAdapter(
+            root.context,
+            android.R.layout.simple_expandable_list_item_1,
+            items
+        )
+        listview.adapter=adapter
+
         return root
     }
 
