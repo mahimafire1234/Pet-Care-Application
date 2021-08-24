39181a261b17df639c67d9e2e34d38ceafead4bd
mahima shrestha <mahimashrestha43@gmail.com>
2021-08-24 19:16:44 +0545

    added maps

--- app/src/main/java/com/mahima/animestreamingapp/ui/AboutusFragment.kt
+++ app/src/main/java/com/mahima/animestreamingapp/ui/AboutusFragment.kt
@@ -6,8 +6,10 @@ import android.view.LayoutInflater
 import android.view.TextureView
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.mahima.animestreamingapp.R
 
@@ -17,6 +19,7 @@ class AboutusFragment : Fragment() {
     private lateinit var imgview:ImageView
     private lateinit var servicetext:TextView
     private lateinit var locationtv:TextView
+    private lateinit var btnopenmaps:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,25 +34,30 @@ class AboutusFragment : Fragment() {
         var view= inflater.inflate(R.layout.fragment_aboutus, container, false)
 //        getting the variable
         paragraphtext = view.findViewById(R.id.paragraphtext)
-        imgview =view.findViewById(R.id.imgview)
         servicetext=view.findViewById(R.id.servicetext)
         locationtv=view.findViewById(R.id.locationtv)
+        btnopenmaps =view.findViewById(R.id.btnopenmaps)
 //        setting the text
-        paragraphtext.setText(" " +
+        paragraphtext.setText(
                 "Jojo is a pet care app for owners that love their pets but have no time for them." +
                 "Jojo helps in organizing care by helping owners shop for their pets and care for their pets."+
                 "We love pets and your pets are our priority."
 
         )
         servicetext.setText("" +
-                "\\u25BA Shop"+
-                "\\u25BA Pet Care"
+                "Shop"+
+                " Pet Care"
         )
-        locationtv.setText("\\u25BA Chabahil, Kathmandu" + "" + "\\u25BA Softwarica sCollege")
+        locationtv.setText("Chabahil, Kathmandu" + "" + " Softwarica sCollege")
 //        adding image to glide
-            Glide.with(this)
-                .load("https://unsplash.com/photos/Hd7vwFzZpH0")
-                .into(imgview)
+//            Glide.with(this)
+//                .load("https://unsplash.com/photos/Hd7vwFzZpH0")
+//                .into(imgview)
+
+//        btnclick
+        btnopenmaps.setOnClickListener {
+            Toast.makeText(view.context,"clicked button",Toast.LENGTH_SHORT).show()
+        }
 
         return view
     }
