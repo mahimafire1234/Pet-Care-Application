48470bd0abd670834c39c7a7caef3e4658851ac7
mahima shrestha <mahimashrestha43@gmail.com>
2021-08-25 12:37:15 +0545

    Created pet database

--- app/src/main/java/com/mahima/animestreamingapp/database/PetProductDatabase.kt
+++ app/src/main/java/com/mahima/animestreamingapp/database/PetProductDatabase.kt
@@ -1,5 +1,41 @@
 package com.mahima.animestreamingapp.database
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.mahima.animestreamingapp.dao.PetProductDAO
+import com.mahima.animestreamingapp.entity.PetProductEntity
 
-abstract class PetProductDatabase {
+@Database(
+    entities = (arrayOf(PetProductEntity::class)),
+    version = 1,
+    exportSchema = false
+)
+abstract class PetProductDatabase: RoomDatabase() {
+
+//    create and instance of dao
+    abstract fun petProductDao() : PetProductDAO
+
+//    companion object
+    companion object{
+        private var instance: PetProductDatabase? = null
+
+         fun getDatabase(context: Context): PetProductDatabase {
+            if (instance == null){
+                synchronized(PetProductDatabase::class){
+                    instance=buildDatabase(context)
+                }
+            }
+            return instance!!
+        }
+
+        private fun buildDatabase(context: Context)=
+            Room.databaseBuilder(
+                context.applicationContext,
+                PetProductDatabase::class.java,
+                "PetDatabase"
+            ).build()
+
+        }
 }
