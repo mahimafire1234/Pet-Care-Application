8c92e2e4ebfde9ac039f7a61058e8dca551a2695
mahima shrestha <mahimashrestha43@gmail.com>
2021-08-26 14:21:09 +0545

    Changes made

---
+++ app/src/main/java/com/mahima/animestreamingapp/ui/home/AddPetFragment.kt
@@ -0,0 +1,23 @@
+package com.mahima.animestreamingapp.ui.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mahima.animestreamingapp.R
+
+class AddPetFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view= inflater.inflate(R.layout.fragment_add_pet, container, false)
+        return view
+    }
+}
--- app/src/main/java/com/mahima/animestreamingapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/mahima/animestreamingapp/ui/home/HomeFragment.kt
@@ -8,6 +8,7 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.mahima.animestreamingapp.R
 import com.mahima.animestreamingapp.databinding.FragmentHomeBinding
 
@@ -15,9 +16,9 @@ class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
     private var _binding: FragmentHomeBinding? = null
+    private lateinit var tvpet : TextView
+    private lateinit var fabaddpet : FloatingActionButton
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -31,7 +32,18 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+//        binding views
+        tvpet = root.findViewById(R.id.tvpet)
+        fabaddpet=root.findViewById(R.id.fabaddpet)
 
+//        open pet form
+        fabaddpet.setOnClickListener {
+            requireActivity().supportFragmentManager.beginTransaction().apply {
+                replace(R.id.layouthome,AddPetFragment())
+                    .addToBackStack(null)
+                    .commit()
+            }
+        }
         return root
     }
 
